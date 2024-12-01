feacb3d500f2dbd3e4e35d2a7a035cf5fa4a8b82
Dmitry Batanov <dima.batanov2019@mail.ru>
2024-12-01 23:26:26 +0300

    version 1.1+objWriter

--- src/main/java/com/example/task3kg/math/Vector2f.java
+++ src/main/java/com/example/task3kg/math/Vector2f.java
@@ -1,6 +1,7 @@
 package com.example.task3kg.math;
 
-// Это заготовка для собственной библиотеки для работы с линейной алгеброй
+import java.util.Objects;
+
 public class Vector2f {
     public Vector2f(float x, float y) {
         this.x = x;
@@ -8,4 +9,24 @@ public class Vector2f {
     }
 
     float x, y;
+
+    public float getX() {
+        return x;
+    }
+
+    public float getY() {
+        return y;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Vector2f vector2f)) return false;
+        return Float.compare(x, vector2f.x) == 0 && Float.compare(y, vector2f.y) == 0;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(x, y);
+    }
 }
