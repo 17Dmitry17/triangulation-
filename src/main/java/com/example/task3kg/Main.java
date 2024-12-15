07a301520befee30931bf5fc9071e932afe341a1
Dmitry Batanov <dima.batanov2019@mail.ru>
2024-12-15 23:19:27 +0300

    v2.1

--- src/main/java/com/example/task3kg/Main.java
+++ src/main/java/com/example/task3kg/Main.java
@@ -10,29 +10,35 @@ import java.nio.file.Path;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        Path fileName = Path.of("C:\\Users\\Home\\Downloads\\CGVSU-main\\CGVSU-main\\3DModels\\SimpleModelsForReaderTests\\Test02.obj");
+        Path fileName = Path.of("C:\\Users\\Home\\Downloads\\CGVSU-main\\CGVSU-main\\3DModels\\SimpleModelsForReaderTests\\Torus.obj");
         String fileContent = Files.readString(fileName);
 
-        System.out.println("Loading model ...");
+        System.out.println("Загрузка ...");
+        System.out.println("-----------------------------");
         TriangulatedModel model = new TriangulatedModel();
         model.vertices = ObjReader.read(fileContent).vertices;
         model.textureVertices = ObjReader.read(fileContent).textureVertices;
         model.normals = ObjReader.read(fileContent).normals;
         model.polygons = ObjReader.read(fileContent).polygons;
 
-        System.out.println("Vertices: " + model.vertices.size());
-        System.out.println("Texture vertices: " + model.textureVertices.size());
-        System.out.println("Normals: " + model.normals.size());
-        System.out.println("Polygons before triangulation: " + model.polygons.size());
+        System.out.println("Вершины: " + model.vertices.size());
+        System.out.println("Вершины текстуры: " + model.textureVertices.size());
+        System.out.println("Нормали: " + model.normals.size());
+        System.out.println("Полигоны : " + model.polygons.size());
 
-        System.out.println("Triangulating model ...");
+        System.out.println("-----------------------------");
+        System.out.println("Триангуляция ...");
+        System.out.println("-----------------------------");
         model.triangulate();
-        System.out.println("Polygons after triangulation: " + model.polygons.size());
-
-        System.out.println("Saving model ...");
+        System.out.println("Вершины: " + model.vertices.size());
+        System.out.println("Вершины текстуры: " + model.textureVertices.size());
+        System.out.println("Нормали: " + model.normals.size());
+        System.out.println("Полигоны : " + model.polygons.size());
+        System.out.println("-----------------------------");
+        System.out.println("Сохранение ...");
         ObjWriter writer = new ObjWriter();
         writer.write(model, "output.obj");
 
-        System.out.println("Model saved to output.obj");
+        System.out.println("Сохранено в output.obj");
     }
 }
